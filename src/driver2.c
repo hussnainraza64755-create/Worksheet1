ea5a247ee3af66fe48220a41d4a247f835d3d914
Muhammad Raza <hussnainraza64755@gmail.com>
2025-12-05 16:22:11 +0000

    Fixed Task 2 Code Logic

--- src/driver2.c
+++ src/driver2.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-void asm_task2(int count);
+void asm_task2(int count, char *name);
 
 int main() {
     int count;
+    char name[100];
+
+    printf("Enter your name: ");
+    scanf("%99s", name);
+
     printf("Enter a number (50-100): ");
     scanf("%d", &count);
-    asm_task2(count);
+
+    asm_task2(count, name);
     return 0;
 }
--- src/task2.asm
+++ src/task2.asm
@@ -2,14 +2,16 @@ global asm_task2
 extern printf
 
 section .data
-    msg_welcome db "Welcome User", 10, 0
+    msg_welcome db "Welcome %s", 10, 0
     msg_error db "Error: Number must be between 50 and 100", 10, 0
 
 section .text
 asm_task2:
     enter 0,0
     pusha
+
     mov ecx, [ebp+8]
+
     cmp ecx, 50
     jle error_label
     cmp ecx, 100
@@ -17,9 +19,12 @@ asm_task2:
 
 loop_start:
     pusha
+
+    push dword [ebp+12]
     push msg_welcome
     call printf
-    add esp, 4
+    add esp, 8
+
     popa
     loop loop_start
     jmp end_label
